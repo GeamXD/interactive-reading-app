8c0a670b1c9842598e987dbc999564d59e77d0dc
Musa Godwin <musa.godwin8112@gmail.com>
2024-11-09 18:45:48 +0000

    pprint

--- pages/5_feedback.py
+++ pages/5_feedback.py
@@ -11,7 +11,7 @@ st.set_page_config(
 st.title('Feedback')
 st.caption('Provide feedback on the accuracy of the spoken text.')
 
-    
+print(st.session_state['recog_eval_report'])
 with st.container(border=True):
     try:
         if st.session_state['reading_report']:
